f7538950e55e8bc3aafb2c78cd2fea27310c7dc2
doktorek1 <757896@gmail.com>
2015-08-13 23:25:45 +0200

    Make a basic User model (with hashed passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,23 +3,19 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: "Example user", email: "user@example.com")
+    @user = User.new(name: "Adam", email: "user@example.com", password: "foobar", password_confirmation:"foobar")
   end
 
   test "should be valid" do
-  	assert @user.valid?
+  	assert @user.valid?, "brak nazwy"
   end
-  test "name should be present" do 
-    @user.name = ""
-    assert_not @user.valid?
-  end
-
-  test "email should be present" do 
-    @user.email = ""
-    assert_not @user.valid?
+  
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?, "#{@user.name} sdfg" 
   end
 
-  test "name should not be too longg" do
+  test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
@@ -29,12 +25,30 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-    end
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+   end
   end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup 
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?    
+  end
+
+  test "password should be nonblank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+  
 end
